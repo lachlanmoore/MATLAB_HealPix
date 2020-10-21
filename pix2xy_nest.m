58734fa3465d43563121c21b6523aa29f60cfa84
Lachlan Moore <lachlanmoore@Lachlans-MacBook-Pro-2.local>
2020-10-21 16:06:16 -0400

    Needs Debugging - Finds Neighbor Nested

---
+++ pix2xy_nest.m
@@ -0,0 +1,22 @@
+function [ix, iy] = pix2xy_nest(nside, ipf)
+
+%Takes pixel input and converts to x and y, nested 
+
+[pix2x, pix2y] = pix2xy;
+
+ipf_tmp = ipf;
+scale = 1;
+ix = 0; iy = 0;
+smax = 2;
+
+for i = 1:smax-1;
+    ip_low = fix(mod(ipf_tmp, 1024));
+    ix = ix + scale * pix2x(ip_low+1);
+    iy = iy + scale * pix2y(ip_low+1);
+    scale = scale*32;
+    ipf_tmp = fix(ipf_tmp/1024);
+end
+ix = ix + scale * pix2x(ipf_tmp+1);
+iy = iy + scale * pix2y(ipf_tmp+1);
+
+end
