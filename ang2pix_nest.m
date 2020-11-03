133a99f25ebdb027684e0d4e2a92e48524bc98ab
Lachlan Moore <lachlanmoore@Lachlans-MacBook-Pro-2.local>
2020-11-03 12:31:59 -0500

    ang2pix_nest

---
+++ ang2pix_nest.m
@@ -0,0 +1,16 @@
+function [ipnest] = ang2pix_nest(nside, theta, phi)
+%Find the pixel index of a given latitude and longitude in the nest scheme
+%
+%Inputs:
+%nside: Pixel resolution, power of 2 less than 30
+%theta, phi: Angular coordinates of a point on a sphere
+%
+%Outputs:
+%ipnest: Pixel index
+
+npix = nside2npix(nside);
+
+ipring = ang2pix_ring(nside, theta, phi);
+ipnest = ring2nest(nside, ipring);
+
+end
